c8f331ff7f2883998014c2bffdb1f79251a8310a
Ekta Sorathia <ektasorathia@gmail.com>
2016-11-23 22:15:24 -0800

    Queue created for login

---
+++ services/login.js
@@ -0,0 +1,20 @@
+
+var db = require("./database/dynamoDB");
+
+exports.doLogin=function(msg, callback) {
+   // var queryJSON = {"username": msg.userId};
+    console.log(msg);
+    console.log("--->>>>");
+    var callbackFunction = function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(result);
+            var jsonResponse={"userDetails":result};
+            //res.customerDetails=result;
+            callback(null, jsonResponse);
+        }
+    }
+    db.findOne("Users", msg, callbackFunction);
+}
