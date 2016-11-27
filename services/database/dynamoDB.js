1937a1ce1aa7481da91567e559d3dfa8fea771b5
gaurav51289 <gauravchodwadia@gmail.com>
2016-11-27 15:44:02 -0800

    Aws key fix

--- services/database/dynamoDB.js
+++ services/database/dynamoDB.js
@@ -1,9 +1,10 @@
 var AWS = require("aws-sdk");
+var key = require('./key');
 
 AWS.config.update({
   region: "us-west-2",
-   accessKeyId: '',  // can omit access key and secret key 
-  secretAccessKey: ''
+  accessKeyId: key.keyJSON.accessKeyId,  // can omit access key and secret key
+  secretAccessKey: key.keyJSON.secretAccessKey
 });
 
 var docClient = new AWS.DynamoDB.DocumentClient()
@@ -20,7 +21,7 @@ exports.findOne = function(tableName,queryJSON,callback){
     docClient.get(params, function(err, data) {
     	if (err) {
         	console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-    	} 
+    	}
     	else {
         	console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
         	callback(null,data);
