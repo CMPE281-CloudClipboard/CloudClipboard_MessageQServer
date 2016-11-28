9e90fe17a606fc2457625c0ae06a80e69e241969
Ekta Sorathia <ektasorathia@gmail.com>
2016-11-27 17:00:20 -0800

    copy service added

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var amqp = require('amqp')
     , util = require('util')
 
 var login = require('./services/login');
+var copypaste = require('./services/copypaste');
 
 var cnn = amqp.createConnection({host:'127.0.0.1'});
 
 cnn.on('ready', function(){
-    console.log("listening on login__signup_queue");
+    console.log("listening on login queue");
     // Gaurav  enter heree
     cnn.queue('LOGIN_QUEUE', function(q){
         q.subscribe(function(message, headers, deliveryInfo, m) {
@@ -15,6 +16,26 @@ cnn.on('ready', function(){
             util.log("DeliveryInfo: " + JSON.stringify(deliveryInfo));
             
                 login.doLogin(message, function (err, res) {
+
+                    //return index sent
+                        cnn.publish(m.replyTo, res, {
+                        contentType: 'application/json',
+                        contentEncoding: 'utf-8',
+                        correlationId: m.correlationId
+                    });
+
+                });
+            
+        });
+    });
+     console.log("listening on copy queue");
+    cnn.queue('COPY_QUEUE', function(q){
+        q.subscribe(function(message, headers, deliveryInfo, m) {
+            util.log(util.format(deliveryInfo.routingKey, message));
+            util.log("Message: " + JSON.stringify(message));
+            util.log("DeliveryInfo: " + JSON.stringify(deliveryInfo));
+            
+                copypaste.copy(message, function (err, res) {
 
                     //return index sent
                         cnn.publish(m.replyTo, res, {
---
+++ services/copypaste.js
@@ -0,0 +1,24 @@
+"use strict"
+var db = require("./database/dynamoDB");
+
+exports.copy=function(msg, callback) {
+   // var queryJSON = {"username": msg.userId};
+    console.log("In copypaste sdfgsdfg function");
+    var date = new Date();
+    var email_ts = msg.email + date;
+    console.log("this");
+    var insertJSON = {"email_timestamp": email_ts,"email": msg.email,"text": msg.text,"fav_flag": msg.fav_flag};
+    console.log("try"+JSON.stringify(insertJSON));
+    var callbackFunction = function (err, result) {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log(result);
+            var jsonResponse={"insertcopyresults":result};
+            //res.customerDetails=result;
+            callback(null, jsonResponse);
+        }
+    }
+    db.insertOne("Clipboard", insertJSON, callbackFunction);
+}
